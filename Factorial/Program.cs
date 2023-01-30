d6f9c8ada74c81fb54a32835c08a975c3b1849b7
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:59:36 -0500

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number to find it's factorial: ");
+            var input = Convert.ToInt32(Console.ReadLine());
+            
+            while (input > 0)
+            {
+                var factorial = input;
+                for (int i = factorial - 1; i > 0; i--)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine(input + "! is " + factorial);
+                break;
+            }
+        }
+    }
+}
